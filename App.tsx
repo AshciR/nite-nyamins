d49b6ce142e7fa82df4523ffb1f96bf5f13885b1
Richard Walker <richard.walker.90@gmail.com>
2024-10-26 18:00:19 -0400

    Layout works on potrait mode

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import HomeScreen from "./HomeScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <HomeScreen/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
