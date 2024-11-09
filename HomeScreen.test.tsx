ab804e13252dac4bd7ce52b7e9dc280c2a7ed61b
Richard Walker <richard.walker.90@gmail.com>
2024-11-09 12:54:49 -0500

    Add Title Toggle

--- HomeScreen.test.tsx
+++ HomeScreen.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react-native';
+import {fireEvent, render, screen, userEvent} from '@testing-library/react-native';
 
 import HomeScreen from "./HomeScreen";
 
@@ -19,6 +19,29 @@ describe('<HomeScreen />', () => {
     // Then: Header should be present
     const vendors = screen.getAllByText(/Vendor \d+/);
     expect(vendors.length).toBe(5)
+  });
+
+  test("Toggle changes title", async () => {
+    const user = userEvent.setup({delay: 1});
+
+    // Given: Home screen has the default title
+    render(<HomeScreen />);
+    const title = screen.getByText('Street Vendor Tracker');
+    expect(title).toBeOnTheScreen();
+
+    // And: The toggle is defaulted to 'on'
+    expect(screen.getByTestId("change-home-screen-title-switch")).toBeChecked()
+
+    // When: We toggle the switch
+    const titleToggle = screen.getByLabelText("change-home-screen-title-toggle")
+    await user.press(titleToggle);
+
+    // Then: The title should change
+    expect(screen.queryByText('Street Vendor Tracker')).not.toBeOnTheScreen()
+    expect(screen.getByText("Nite Nyammins")).toBeOnTheScreen()
+
+    // And: The toggle is off
+    expect(screen.getByTestId("change-home-screen-title-switch")).not.toBeChecked()
   });
 
 });
