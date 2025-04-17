23f7cf4f212f3aaf55b7771893104595de18ba28
Richard Walker <richard.walker.90@gmail.com>
2025-04-16 19:54:14 -0500

    Create iOS icon

--- app.config.ts
+++ app.config.ts
@@ -3,11 +3,11 @@ import {ExpoConfig} from 'expo/config';
 export default (): ExpoConfig => {
 
   return {
-    "name": "nite-nyamins",
+    "name": "Nyamins",
     "slug": "nite-nyamins",
     "version": "1.0.0",
     "orientation": "default",
-    "icon": "./assets/icon.png",
+    "icon": "./assets/app-icon.png",
     "userInterfaceStyle": "light",
     "newArchEnabled": true,
     "splash": {
